ab62b098ab9ba28fdba03ebb5b4aed47cde1a0e5
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 10:44:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,19 +27,19 @@ def long_planeteer_calls(planet)# code an argument here
 
 end
 
-cheese_types = ["cheddar", "gouda", "camembert"]
+
 def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
 
-
- myArray = cheese
+  myArray = cheese
      myArray.each {|n|
                     i = 0
                     while i < cheese_types.count
                      if n != cheese_types[i]
                         i += 1
                       else
-                        return cheese_types [i]
+                        return cheese_types[i]
                         i += 1
                       end
 
