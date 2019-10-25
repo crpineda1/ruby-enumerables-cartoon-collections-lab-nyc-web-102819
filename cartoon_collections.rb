5df458793a7eb661dc1ccbbcadacf8216ba0b776
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 10:13:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,8 +38,9 @@ def find_the_cheese(cheese)# code an argument here
                     while i < cheese.count
                       if n == cheese_types[i]
                         p cheese_type [i]
-                      i += 1
+                      
                     end
+                    i += 1
 
                     }
 
